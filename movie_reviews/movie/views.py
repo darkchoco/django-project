b4fc236a8340b2c4dcede09f1563113c1f34abf3
darkchoco <darkchoco.ice@gmail.com>
2022-09-26 17:51:55 +0200

    main: [movie_reviews] 'movie' app implemented.

---
+++ movie_reviews/movie/views.py
@@ -0,0 +1,23 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Movie
+
+
+# Create your views here.
+def home(request):
+    search_term = request.GET.get('searchMovie')
+    if search_term:
+        movies = Movie.objects.filter(title__icontains=search_term)
+    else:
+        movies = Movie.objects.all()
+
+    return render(request, 'home.html', {'search_term': search_term, 'movies': movies})
+
+
+def about(request):
+    return HttpResponse('<h1>This is the About page</h1>')
+
+
+def signup(request):
+    email = request.GET.get('email')
+    return render(request, 'signup.html', {'email': email})
