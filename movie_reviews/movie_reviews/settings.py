1f838d8b3a1d340f84bcdc4706d79068ce241822
darkchoco <darkchoco@cloudscape.kr>
2022-11-09 14:57:26 +0100

    main: [movie_reviews] base template set.

--- movie_reviews/movie_reviews/settings.py
+++ movie_reviews/movie_reviews/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'movie_reviews.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'movie_reviews/templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
