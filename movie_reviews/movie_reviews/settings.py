22bf4e83e946c695523b2e3eee31006dff717977
darkchoco <darkchoco@cloudscape.kr>
2022-11-09 17:28:07 +0100

    main: [movie_reviews] static files added - closes #4

--- movie_reviews/movie_reviews/settings.py
+++ movie_reviews/movie_reviews/settings.py
@@ -134,3 +134,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / 'movie_reviews/static/',
+]
